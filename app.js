de3107e78a75e2e66465c42809ce1d740ca53879
BommishettyVamshi <bvamshivamshi22@gmail.com>
2024-12-29 13:44:41 +0000

    Initialization of Database and Server on port 3000 of url:https://vamshincue4njscpfvwof.drops.nxtwave.tech is done, the code of the this initialization process is there in app.js of coding-practin-9a project repository

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+
+const app = express();
+const bcrypt = require('bcrypt')
+
+const dbPath = path.join(__dirname, 'userData.db')
+
+let db = null
+
+const initializeDBAndServer = async () => {
+    try{
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        })
+
+        app.listen(3000,() => {
+            console.log('server is runnint at https://vamshincue4njscpfvwof.drops.nxtwave.tech..')
+        })
+    }catch(e){
+        console.log(`DB Error: ${e.message}`)
+    }
+}
+
+initializeDBAndServer();
