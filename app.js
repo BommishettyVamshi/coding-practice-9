f39d215634afc74c35e8491ae43660f445e8efb0
BommishettyVamshi <bvamshivamshi22@gmail.com>
2024-12-29 15:01:31 +0000

    created and implemented login API successfully in app.js for login user

--- app.js
+++ app.js
@@ -36,21 +36,47 @@ app.post('/register/', async (req, res) => {
   try {
     const {username, name, password, gender, location} = req.body
 
-  
     const selectQuery = `select * from user where username = '${username}';`
     const dbUser = await db.get(selectQuery)
 
     if (dbUser !== undefined) {
       res.status(400).send('User already exists')
-    } else if (password.length < 5) {
-      res.status(400).send('Password is too short')
     } else {
-      const createQuery = `
-                insert into user (username,name,password,gender,location) 
-                values ('${username}','${name}','${hashedPassword}','${gender}','${location}');
-            `
-      const dbUserId = await db.run(createQuery)
-      res.status(400).send(`User created successfully`)
+      if (password.length < 5) {
+        res.status(400).send('Password is too short')
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const createQuery = `
+          insert into user (username,name,password,gender,location) 
+          values ('${username}','${name}','${hashedPassword}','${gender}','${location}');
+        `
+        const dbUserId = await db.run(createQuery)
+        res.status(400).send(`User created successfully`)
+        console.log(`User is created with id : ${dbUserId.lastID}`)
+      }
+    }
+  } catch (e) {
+    console.log(`Request Error: ${e.message}`)
+  }
+})
+
+//API 2
+app.post('/login/', async (req, res) => {
+  try {
+    const {username, password} = req.body
+    const selectQuery = `select * from user where username = '${username}';`
+    const dbUser = await db.get(selectQuery)
+
+    if (dbUser === undefined) {
+      res.status(400).send('Invalid user')
+    } else {
+      const isPasswordMatched = await bcrypt.compare(password, dbUser.password)
+
+      if (isPasswordMatched) {
+        res.status(200).send('Login success!')
+      } else {
+        res.status(400).send('Invalid password')
+      }
     }
   } catch (e) {
     console.log(`Request Error: ${e.message}`)
